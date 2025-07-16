375e73f9897c8b3ff762376de125df22e5f8e828
Andrew Bell <andrew.bell@kyndryl.com>
2025-07-16 11:17:38 +0100

    some python info

---
+++ test_sample.py
@@ -0,0 +1,18 @@
+import pytest
+
+from sample import add, minus, divide
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(14, 15) == 29
+
+def test_minus():
+    assert minus(5, 3) == 2
+    assert minus(6, 9) == -3
+    assert minus(11, 4) != 4
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(5, 2) == 2.5
+    with pytest.raises(ValueError, match='Cannot divide by zero!'):
+        divide(4, 0)
